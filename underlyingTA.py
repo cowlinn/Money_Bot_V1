8071fc4a2ca56b705904a9ebc53f2e51f112abac
Seow Yang Han, Darryl <132463288+DarrylSeow@users.noreply.github.com>
2023-06-11 21:06:53 +0800

    underlyingTA.py ready to be called (#35)
    * underlyingTA.py ready to be called
    
    * Update underlyingTA.py

---
+++ underlyingTA.py
@@ -0,0 +1,121 @@
+import pandas as pd
+import ta_lib
+import talib
+import yfinance as yf
+import weights_optimisation
+import sys
+
+"""
+how to call:
+    - by importing:
+        
+        import underlyingTA
+        stock_name = 'SPY'
+        trades = underlyingTA.decision(stock_name)
+        
+        or
+        
+        stock_lst = ['SPY', 'AAPL', 'TSLA', 'MSFT', 'AMZN', 'GOOGL', 'NVDA', 'MA', 'V']
+        for i in stock_lst:
+            underlyingTA.decision(i) # it will just write a bunch of log files
+            
+
+    - by running from bash script or command line or something?
+        idk what we are gonna use so the example below is some code for a bash script (btw I can't rmb if bash will parse arguments as strings by default?)
+        
+        for ticker in SPY AAPL TSLA MSFT AMZN GOOGL NVDA MA V;
+            do ./underlyingTA.py $ticker 4d 15m;
+        done
+    
+        ok I did some googling and tried it out, for powershell its:
+        
+        python underlyingTA.py SPY 4d 15m
+        
+        in place of the ./underlyingTA.py $ticker 4d 15m
+        
+        # if we don't want to call it in this way just comment off the if statements at the bottom
+        
+        
+# note: if you do the list of tickers thing, it will take a long ass time because it does it sequentially.
+        Should probably split it into multiple loops.
+"""
+# make a trading decision based on current available market information
+def decision(stock_name, data_period = '4d', resolution = '15m'):
+    calls = {}
+    puts = {}
+
+    # call the data
+    stock = yf.Ticker(stock_name)  # this goes in main()
+    data = stock.history(period = data_period, interval = resolution) # historical price data
+    data.reset_index(inplace=True) # converts datetime to a column
+    data['ATR'] = talib.ATR(data['High'], data['Low'], data['Close'])
+    current_data = data.iloc[-1] 
+    current_date = str(data['Datetime'].iloc[-1]).split()[0]
+    min_samples = int(data_period[:-1]) # average of 1 trade per day?
+    
+    # get optimised weights by backtesting through the data_period. Weights have precision of 0.1
+    optimised_weights = weights_optimisation.optimise(data, interval = 0.1, min_sample_size = min_samples)
+    backtest_results = weights_optimisation.backtest(data, optimised_weights)
+    if backtest_results[0] < 50:
+        return 0 # do nothing if the current strategy is unlikely to work (has winrate < 50%)
+    
+    # get current trading signal using the weights
+    unweighted_signals = ta_lib.TA(data)
+    Nweights = len(unweighted_signals.iloc[0]) # number of weights required according to ta_lib.py (basically how many indactors we are using)
+    overall_signal = 0
+    for i in range(Nweights):
+        overall_signal += optimised_weights[i]*unweighted_signals.iloc[0][i] # apply the weights
+
+    # make the decisions based on the same thresholds used in weights_optimisation.py
+    if overall_signal > 0.4:
+        # price predicted to go up
+        # print ('Buy a call at '+str(current_data['Datetime']))
+        stoploss = current_data['Close']-current_data['ATR']*2
+        takeprofit = current_data['Close']+current_data['ATR']*2.5
+        """
+        Execute trade here? app.buy(call)
+        Or we can just have the optionsTA.py file read the suggested trade actions in the log file and see if got any good options?
+        """
+        # write trade actions to a file
+        filename = 'Trade-action-logs/'+current_date + '_' + stock_name +'_call-logs.txt'
+        call_file = open(filename, "a")
+        # data is written in the format of transaction timestamp, underlying price at time of purchase, stoploss, takeprofit
+        call_file.write(str(current_data['Datetime']) + '    ' + str(current_data['Close']) + '    ' + str(stoploss) + '    ' + str(takeprofit))
+        call_file.close()
+        calls[str(current_data['Datetime'])] = (current_data['Close'], stoploss, takeprofit)
+
+        
+    elif overall_signal < -0.4:
+        # print('Buy a put at '+str(current_data['Datetime']))
+        stoploss = current_data['Close']+current_data['ATR']*2
+        takeprofit = current_data['Close']-current_data['ATR']*2.5
+        """
+        Execute trade here? app.buy(put)
+        Or we can just have the optionsTA.py file read the suggested trade actions in the log file and see if got any good options?
+        """
+        # write trade actions to a file
+        filename = 'Trade-action-logs/'+current_date + '_' + stock_name +'_put-logs.txt'
+        put_file = open(filename, "a")
+        # data is written in the format of transaction timestamp, underlying price at time of purchase, stoploss, takeprofit
+        put_file.write(str(current_data['Datetime']) + '    ' + str(current_data['Close']) + '    ' + str(stoploss) + '    ' + str(takeprofit))
+        put_file.close()
+        puts[str(current_data['Datetime'])] = (current_data['Close'], stoploss, takeprofit)
+
+        """"
+        Assume that the stoploss/take profit is set in the broker and it will auto close the position if either one is hit
+        """
+    print('DONE!!')
+    return calls,puts # in case we want to just call this script directly from another python script
+
+if len(sys.argv) != 4:
+    print("Positional arguments:")
+    print("(i) Ticker name")
+    print("(ii) Data lookback period")
+    print("(iii) Resolution of data")
+    sys.exit()
+else:
+    results = decision(sys.argv[1], sys.argv[2], sys.argv[3])
+    print(results)
+    sys.exit()
+
+  
--- weights_optimisation.py
+++ weights_optimisation.py
@@ -5,8 +5,9 @@ import yfinance as yf
 
 """
 how to call:
-    initial = initial_optimise(data, interval = 0.1)
-    second = second_optimise(data, interval = 0.1, fixed_weight = initial[0], weight_number = initial[1])
+    optimise(data, interval=0.1, min_sample_size=5) # gives an optimised ordered list of weights
+or: 
+    backtest(data, optimise(data, interval=0.1, min_sample_size=5))
 
 for rael bot:
     just take the optimised weights, copy the code in backtest and just take the latest readings to output some trade action (repplace the buy/sell prints)
@@ -15,20 +16,24 @@ Some Results:
 - the usefulness of each indicator varies wildly??
 - we cannot just optimise for winrate - if the losses are much larger than the wins, we will still loose money
 - we may need one more optimisation function?
-- two optimised results (backtest(data, 0.125, 0, 0.125, 0.5, 0.125) and backtest(data, 0.1, 0.1, 0.4, 0.3, 0.1))
+- two single optimised results (backtest(data, 0.125, 0, 0.125, 0.5, 0.125)) and backtest(data, 0.1, 0.1, 0.4, 0.3, 0.1))
   gave identical winrates but very different gains, for the same number of trades over 10 days
 - using 30 days of 15m data, backtest(data, 0.1, 0.3, 0.3, 0, 0.3) gives an insane result of (74.36, 56.2188720703125, 39)
   74% winrate over 39 trades plus a HUGE gain??
 """
 
-stock_name = "SPY"
-data_period = "30d"
-resolution = "15m"
-stock = yf.Ticker(stock_name)  # this goes in main()
-data = stock.history(period = data_period, interval = resolution) # historical price data
+# stock_name = "SPY"
+# data_period = "4d"
+# resolution = "15m"
+# stock = yf.Ticker(stock_name)  # this goes in main()
+# data = stock.history(period = data_period, interval = resolution) # historical price data
 
-test = []
-def backtest(data, weight1 = 0.2, weight2 = 0.2, weight3 = 0.2, weight4 = 0.2, weight5 = 0.2):
+def backtest(data, ordered_weights): # ordered_weights is a list of weights in the same order as the output of ta_lib.TA()
+    weights = {}
+    Nweights = len(ta_lib.TA(data).iloc[0]) # number of weights required according to ta_lib.py (basically how many indactors we are using)
+    if len(ordered_weights) != Nweights: # check if the input weights are valid
+        print('Incorrect number of weights!')
+        return
     data = data.copy()
     data.reset_index(inplace=True) # converts datetime to a column
     data['ATR'] = talib.ATR(data['High'], data['Low'], data['Close'])
@@ -43,21 +48,23 @@ def backtest(data, weight1 = 0.2, weight2 = 0.2, weight3 = 0.2, weight4 = 0.2, w
         current_hour = int(str(current_data['Datetime']).split()[1][0:2])
         current_minute = int(str(current_data['Datetime']).split()[1][3:5])
         current = ta_lib.TA(data.loc[:i])
-        output = weight1*current.iloc[0][0] + weight2*current.iloc[0][1]+weight3*current.iloc[0][2]+weight4*current.iloc[0][3]+weight5*current.iloc[0][4]
+        output = 0
+        for i in range(Nweights):
+            output += ordered_weights[i]*current.iloc[0][i] # apply the weights
         # if current_hour > 13: # do not enter trades after 2pm
         #     # print(current_data['Datetime'])
         #     continue
         # opening positions
-        if output > 0.3:
+        if output > 0.4:
             # print ('Buy a call at '+str(current_data['Datetime']))
-            stoploss = current_data['Close']-current_data['ATR']*1.75
-            takeprofit = current_data['Close']+current_data['ATR']*2
+            stoploss = current_data['Close']-current_data['ATR']*2
+            takeprofit = current_data['Close']+current_data['ATR']*2.5
             calls[str(current_data['Datetime'])] = (current_data['Close'], stoploss, takeprofit)
             total_trades += 1
-        elif output < -0.3:
+        elif output < -0.4:
             # print('Buy a put at '+str(current_data['Datetime']))
-            stoploss = current_data['Close']+current_data['ATR']*1.75
-            takeprofit = current_data['Close']-current_data['ATR']*2
+            stoploss = current_data['Close']+current_data['ATR']*2
+            takeprofit = current_data['Close']-current_data['ATR']*2.5
             puts[str(current_data['Datetime'])] = (current_data['Close'], stoploss, takeprofit)
             total_trades += 1
         
@@ -69,15 +76,15 @@ def backtest(data, weight1 = 0.2, weight2 = 0.2, weight3 = 0.2, weight4 = 0.2, w
                 # stoploss triggered, close the position
                 losses += 1
                 gains += current_data['Close']-trade_info[0] # count the profit/loss
-                print('It is currently ' + str(current_data['Datetime']) + '. Sell the call purchased at ' + i)
-                print(trade_info[0]-current_data['Close'] ,'baaasly trade')
+                # print('It is currently ' + str(current_data['Datetime']) + '. Sell the call purchased at ' + i)
+                # print(trade_info[0]-current_data['Close'] ,'baaasly trade')
                 remove.append(i)
             elif current_data['Close'] > trade_info[2]:
                 # takeprofit reached, close the position
                 wins += 1
                 gains += current_data['Close']-trade_info[0] # count the profit/loss
-                print('It is currently ' + str(current_data['Datetime']) + '. Sell the call purchased at ' + i)
-                print(current_data['Close']-trade_info[0],'guuuud trade')
+                # print('It is currently ' + str(current_data['Datetime']) + '. Sell the call purchased at ' + i)
+                # print(current_data['Close']-trade_info[0],'guuuud trade')
                 remove.append(i)
         for i in remove:
                 # remove closed positions from dictionary of open positions
@@ -90,162 +97,114 @@ def backtest(data, weight1 = 0.2, weight2 = 0.2, weight3 = 0.2, weight4 = 0.2, w
                 # takeprofit reached, close the position
                 wins += 1
                 gains += trade_info[0]-current_data['Close'] # count the profit/loss
-                print('It is currently ' + str(current_data['Datetime']) + '. Sell the put purchased at ' + i)
-                print(trade_info[0]-current_data['Close'] ,'guuuud trade')
+                # print('It is currently ' + str(current_data['Datetime']) + '. Sell the put purchased at ' + i)
+                # print(trade_info[0]-current_data['Close'] ,'guuuud trade')
                 remove.append(i)
             elif current_data['Close'] > trade_info[1]:
                 # stoploss triggered, close the position
                 losses += 1
                 gains += trade_info[0]-current_data['Close'] # count the profit/loss
-                print('It is currently ' + str(current_data['Datetime']) + '. Sell the put purchased at ' + i)
-                print(trade_info[0]-current_data['Close'] ,'baaasly trade')
+                # print('It is currently ' + str(current_data['Datetime']) + '. Sell the put purchased at ' + i)
+                # print(trade_info[0]-current_data['Close'] ,'baaasly trade')
                 remove.append(i)
         for i in remove:
                 # remove closed positions from dictionary of open positions
                 puts.pop(i)
     completed_trades = wins + losses
-    if completed_trades == total_trades:
-        print('All trades completed')
+    # if completed_trades == total_trades:
+        # print('All', completed_trades, 'trades completed')
+    if wins == 0:
+        winrate = 0
+        return winrate, gains, completed_trades
     winrate = round(wins/completed_trades*100, 2)
     return winrate, gains, completed_trades #, calls, puts
 
-def initial_optimise(data, interval, Nweights = 5, min_sample_size = 15):
+def initial_optimise(data, interval, min_sample_size = 15):
     test_log = {}
     iterr = range(0,int(1/interval))
-    #Nweights = 5 # number of weights
+    Nweights = len(ta_lib.TA(data).iloc[0]) # number of weights required according to ta_lib.py (basically how many indactors we are using)
     for i in iterr:
-        test_weight = i*interval
-        normalised_weights = (1-test_weight)/(Nweights-1) # distribute the remaining weight equally
-        name1 = str(test_weight)+' 1'
-        name2 = str(test_weight)+' 2'
-        name3 = str(test_weight)+' 3'
-        name4 = str(test_weight)+' 4'
-        name5 = str(test_weight)+' 5'
-        result1 = backtest(data, test_weight, normalised_weights, normalised_weights, normalised_weights, normalised_weights)
-        result2 = backtest(data, normalised_weights, test_weight, normalised_weights, normalised_weights, normalised_weights)
-        result3 = backtest(data, normalised_weights, normalised_weights, test_weight, normalised_weights, normalised_weights)
-        result4 = backtest(data, normalised_weights, normalised_weights, normalised_weights, test_weight, normalised_weights)
-        result5 = backtest(data, normalised_weights, normalised_weights, normalised_weights, normalised_weights, test_weight)
-        # only add significant samples to find highest winrate
-        if result1[2] > min_sample_size:
-            test_log[name1] = result1[0]
-        if result2[2] > min_sample_size:
-            test_log[name2] = result2[0]
-        if result3[2] > min_sample_size:
-            test_log[name3] = result3[0]
-        if result4[2] > min_sample_size:
-            test_log[name4] = result4[0]
-        if result5[2] > min_sample_size:
-            test_log[name5] = result5[0]
-    best_weight = max(test_log, key = test_log.get)
-    result = best_weight.split()
-    result[0] = float(result[0])
-    result[1] = int(result[1])
-    return result
+        for k in range(1, 1+Nweights):
+            test_weight = i*interval
+            name = str(test_weight) + ' ' + str(k)
+            result = backtest(data, interpret([(test_weight, k)], data))
+            if result[2] >= min_sample_size and result[1] > 0: # only consider results with a min sample size and actual profit
+                test_log[name] = result[0]
+    if test_log:
+        best_weight = max(test_log, key = test_log.get)
+        result = best_weight.split()
+        result[0] = float(result[0])
+        result[1] = int(result[1])
+        return [tuple(result)]
+    else:
+        # no suitable optimisations found. not a good idea to trade.
+        min_sample_size -= 1
+        return initial_optimise(data, interval, min_sample_size) # reduce sample size requirement until some prediction is made?
 
-def second_optimise(data, interval, fixed_weight, weight_number, min_sample_size = 15):
+def second_optimise(data, interval, initial_results, min_sample_size = 15):
+    fixed_weight_number_lst = []
+    fixed_weight_dict = {}
+    for i in range(len(initial_results)):
+        fixed_weight_number_lst.append(initial_results[i][1]) # which weights have been fixed?
+        fixed_weight_dict[initial_results[i][1]] = initial_results[i][0]
     test_log = {}
-    iterr = range(0,int((1-fixed_weight)/interval)+1)
-    for i in iterr:
-        test_weight = i*interval
-        normalised_weights = (1-test_weight-fixed_weight)/3
-        
-        if weight_number == 1:
-            name2 = str(test_weight)+' 2'
-            name3 = str(test_weight)+' 3'
-            name4 = str(test_weight)+' 4'
-            name5 = str(test_weight)+' 5'
-            result2 = backtest(data, fixed_weight, test_weight, normalised_weights, normalised_weights, normalised_weights)
-            result3 = backtest(data, fixed_weight, normalised_weights, test_weight, normalised_weights, normalised_weights)
-            result4 = backtest(data, fixed_weight, normalised_weights, normalised_weights, test_weight, normalised_weights)
-            result5 = backtest(data, fixed_weight, normalised_weights, normalised_weights, normalised_weights, test_weight)
-            if result2[2] > min_sample_size:
-                test_log[name2] = result2[0]
-            if result3[2] > min_sample_size:
-                test_log[name3] = result3[0]
-            if result4[2] > min_sample_size:
-                test_log[name4] = result4[0]
-            if result5[2] > min_sample_size:
-                test_log[name5] = result5[0]
+    total_fixed_weight = sum(fixed_weight_dict.values())
+    iterr = range(0,int((1-total_fixed_weight)/interval)+1)
+    Nweights = len(ta_lib.TA(data).iloc[0]) # number of weights required according to ta_lib.py (basically how many indactors we are using)
+    for k in range(1, 1+Nweights):
+        if (k in fixed_weight_number_lst):
+            continue
+        for i in iterr:
+            test_weight = i*interval
+            name = str(test_weight) + ' ' + str(k)
+            copy = initial_results.copy()
+            copy.append((test_weight, k))
+            input_weights = interpret(copy, data)
+            result = backtest(data, input_weights)
+            if result[2] >= min_sample_size and result[1] > 0:
+                test_log[name] = result[0]
+    if test_log:
+        best_weight = max(test_log, key = test_log.get)
+        print(test_log[best_weight], best_weight)
+        second_lst = best_weight.split()
+        second_lst[0] = float(second_lst[0])
+        second_lst[1] = int(second_lst[1])
+        final = initial_results.copy()
+        final.append(tuple(second_lst)) # this changes the original result too (can use a shallow copy if this is not desired)
+        return final
+    else:
+        # no suitable optimisations found. not a good idea to trade.
+        min_sample_size -= 1
+        return second_optimise(data, interval, initial_results, min_sample_size) # reduce sample size requirement until some prediction is made?
 
-        if weight_number == 2:
-            name1 = str(test_weight)+' 1'
-            name3 = str(test_weight)+' 3'
-            name4 = str(test_weight)+' 4'
-            name5 = str(test_weight)+' 5'
-            result1 = backtest(data, test_weight, fixed_weight, normalised_weights, normalised_weights, normalised_weights)
-            result3 = backtest(data, normalised_weights, fixed_weight, test_weight, normalised_weights, normalised_weights)
-            result4 = backtest(data, normalised_weights, fixed_weight, normalised_weights, test_weight, normalised_weights)
-            result5 = backtest(data, normalised_weights, fixed_weight, normalised_weights, normalised_weights, test_weight)
-            if result1[2] > min_sample_size:
-                test_log[name1] = result1[0]
-            if result3[2] > min_sample_size:
-                test_log[name3] = result3[0]
-            if result4[2] > min_sample_size:
-                test_log[name4] = result4[0]
-            if result5[2] > min_sample_size:
-                test_log[name5] = result5[0]
+def optimise(data, interval, min_sample_size):
+    Nweights = len(ta_lib.TA(data).iloc[0]) # number of weights required according to ta_lib.py (basically how many indactors we are using)
+    initial = initial_optimise(data, interval, min_sample_size)
+    second= second_optimise(data, interval, initial, min_sample_size)
+    for i in range(Nweights-3):
+        # just to know how many times to loop
+        second = second_optimise(data, interval, second, min_sample_size)
+    return interpret(second, data)
 
-        if weight_number == 3:
-            name1 = str(test_weight)+' 1'
-            name2 = str(test_weight)+' 2'
-            name4 = str(test_weight)+' 4'
-            name5 = str(test_weight)+' 5'
-            result1 = backtest(data, test_weight, normalised_weights, fixed_weight, normalised_weights, normalised_weights)
-            result2 = backtest(data, normalised_weights, test_weight, fixed_weight, normalised_weights, normalised_weights)
-            result4 = backtest(data, normalised_weights, normalised_weights, fixed_weight, test_weight, normalised_weights)
-            result5 = backtest(data, normalised_weights, normalised_weights, fixed_weight, normalised_weights, test_weight)
-            if result1[2] > min_sample_size:
-                test_log[name1] = result1[0]
-            if result2[2] > min_sample_size:
-                test_log[name2] = result2[0]
-            if result4[2] > min_sample_size:
-                test_log[name4] = result4[0]
-            if result5[2] > min_sample_size:
-                test_log[name5] = result5[0]
-
-        if weight_number == 4:
-            name1 = str(test_weight)+' 1'
-            name2 = str(test_weight)+' 2'
-            name3 = str(test_weight)+' 3'
-            name5 = str(test_weight)+' 5'
-            result1 = backtest(data, test_weight, normalised_weights, normalised_weights, fixed_weight, normalised_weights)
-            result2 = backtest(data, normalised_weights, test_weight, normalised_weights, fixed_weight, normalised_weights)
-            result3 = backtest(data, normalised_weights, normalised_weights, test_weight, fixed_weight, normalised_weights)
-            result5 = backtest(data, normalised_weights, normalised_weights, normalised_weights, fixed_weight, test_weight)
-            if result1[2] > min_sample_size:
-                test_log[name1] = result1[0]
-            if result2[2] > min_sample_size:
-                test_log[name2] = result2[0]
-            if result3[2] > min_sample_size:
-                test_log[name3] = result3[0]
-            if result5[2] > min_sample_size:
-                test_log[name5] = result5[0]
-    
-        if weight_number == 5:
-            name1 = str(test_weight)+' 1'
-            name2 = str(test_weight)+' 2'
-            name3 = str(test_weight)+' 3'
-            name4 = str(test_weight)+' 4'
-            result1 = backtest(data, test_weight, normalised_weights, normalised_weights, normalised_weights, fixed_weight)
-            result2 = backtest(data, normalised_weights, test_weight, normalised_weights, normalised_weights, fixed_weight)
-            result3 = backtest(data, normalised_weights, normalised_weights, test_weight, normalised_weights, fixed_weight)
-            result4 = backtest(data, normalised_weights, normalised_weights, normalised_weights, test_weight, fixed_weight)
-            if result1[2] > min_sample_size:
-                test_log[name1] = result1[0]
-            if result2[2] > min_sample_size:
-                test_log[name2] = result2[0]
-            if result3[2] > min_sample_size:
-                test_log[name3] = result3[0]
-            if result4[2] > min_sample_size:
-                test_log[name4] = result4[0]
-    best_weight = max(test_log, key = test_log.get)
-    print(test_log[best_weight], best_weight)
-    second_lst = best_weight.split()
-    second_lst[0] = float(second_lst[0])
-    second_lst[1] = int(second_lst[1])
-    second = [(fixed_weight, weight_number), tuple(second_lst)]
-    return second
-       
-
+def interpret(optimised_output, data):
+    Nweights = len(ta_lib.TA(data).iloc[0]) # number of weights required according to ta_lib.py (basically how many indactors we are using)
+    fixed_weight = 0
+    Nfixed = len(optimised_output)
+    fixed_weight_lst = []
+    fixed_weight_dict = {}
+    for i in range(len(optimised_output)):
+        fixed_weight += optimised_output[i][0]
+        fixed_weight_lst.append(optimised_output[i][1])
+        fixed_weight_dict[optimised_output[i][1]] = optimised_output[i][0]
+    normalised_weight = (1-fixed_weight)/(Nweights-Nfixed)
+    dic = {}
+    for i in range(1,Nweights+1):
+        if (i in fixed_weight_lst):
+            dic[i] = fixed_weight_dict[i]
+        else:
+            dic[i] = normalised_weight
+    ordered_weights = []
+    for i in dic:
+        ordered_weights.append(dic[i])
+    return ordered_weights
 
