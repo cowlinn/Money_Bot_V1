d64705654f306101197cee5ea60521bb59ca7d98
Seow Yang Han, Darryl <darryl.seow98@gmail.com>
2023-06-11 21:05:31 +0800

    Update underlyingTA.py

--- underlyingTA.py
+++ underlyingTA.py
@@ -3,11 +3,42 @@ import ta_lib
 import talib
 import yfinance as yf
 import weights_optimisation
+import sys
 
-# stock_name = "SPY"
-# data_period = "4d"
-# resolution = "15m"
+"""
+how to call:
+    - by importing:
+        
+        import underlyingTA
+        stock_name = 'SPY'
+        trades = underlyingTA.decision(stock_name)
+        
+        or
+        
+        stock_lst = ['SPY', 'AAPL', 'TSLA', 'MSFT', 'AMZN', 'GOOGL', 'NVDA', 'MA', 'V']
+        for i in stock_lst:
+            underlyingTA.decision(i) # it will just write a bunch of log files
+            
 
+    - by running from bash script or command line or something?
+        idk what we are gonna use so the example below is some code for a bash script (btw I can't rmb if bash will parse arguments as strings by default?)
+        
+        for ticker in SPY AAPL TSLA MSFT AMZN GOOGL NVDA MA V;
+            do ./underlyingTA.py $ticker 4d 15m;
+        done
+    
+        ok I did some googling and tried it out, for powershell its:
+        
+        python underlyingTA.py SPY 4d 15m
+        
+        in place of the ./underlyingTA.py $ticker 4d 15m
+        
+        # if we don't want to call it in this way just comment off the if statements at the bottom
+        
+        
+# note: if you do the list of tickers thing, it will take a long ass time because it does it sequentially.
+        Should probably split it into multiple loops.
+"""
 # make a trading decision based on current available market information
 def decision(stock_name, data_period = '4d', resolution = '15m'):
     calls = {}
@@ -22,7 +53,7 @@ def decision(stock_name, data_period = '4d', resolution = '15m'):
     current_date = str(data['Datetime'].iloc[-1]).split()[0]
     min_samples = int(data_period[:-1]) # average of 1 trade per day?
     
-    # get optimised weights by backtesting through the data_period
+    # get optimised weights by backtesting through the data_period. Weights have precision of 0.1
     optimised_weights = weights_optimisation.optimise(data, interval = 0.1, min_sample_size = min_samples)
     backtest_results = weights_optimisation.backtest(data, optimised_weights)
     if backtest_results[0] < 50:
@@ -73,4 +104,18 @@ def decision(stock_name, data_period = '4d', resolution = '15m'):
         """"
         Assume that the stoploss/take profit is set in the broker and it will auto close the position if either one is hit
         """
+    print('DONE!!')
     return calls,puts # in case we want to just call this script directly from another python script
+
+if len(sys.argv) != 4:
+    print("Positional arguments:")
+    print("(i) Ticker name")
+    print("(ii) Data lookback period")
+    print("(iii) Resolution of data")
+    sys.exit()
+else:
+    results = decision(sys.argv[1], sys.argv[2], sys.argv[3])
+    print(results)
+    sys.exit()
+
+  
